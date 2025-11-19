d64442ce1101638ccf50b335bb1ebc846fbb5f8d
Andy Keller <andy@barrfitz.com>
2025-11-18 20:45:28 -0700

    feat: wip

--- module/helpers/utils.mjs
+++ module/helpers/utils.mjs
@@ -3,12 +3,20 @@
  */
 export const preloadHandlebarsTemplates = async function () {
     return loadTemplates([
-      'modules/masks-newgeneration-unofficial/templates/sheets/actor-sheet.hbs',
-      'modules/masks-newgeneration-unofficial/templates/parts/actor-header.hbs',
-      'modules/masks-newgeneration-unofficial/templates/parts/actor-attributes.hbs',
-      'modules/masks-newgeneration-unofficial/templates/parts/actor-movelist.hbs',
-      'modules/masks-newgeneration-unofficial/templates/parts/actor-social.hbs',
-      'modules/masks-newgeneration-unofficial/templates/parts/actor-advancement.hbs',
-      'modules/masks-newgeneration-unofficial/templates/influences-tab-page.hbs',
-    ]);
+					"modules/masks-newgeneration-unofficial/templates/sheets/actor-sheet.hbs",
+					"modules/masks-newgeneration-unofficial/templates/parts/actor-header.hbs",
+					"modules/masks-newgeneration-unofficial/templates/parts/actor-attributes.hbs",
+					"modules/masks-newgeneration-unofficial/templates/parts/actor-movelist.hbs",
+					"modules/masks-newgeneration-unofficial/templates/parts/actor-social.hbs",
+					"modules/masks-newgeneration-unofficial/templates/parts/actor-advancement.hbs",
+					"modules/masks-newgeneration-unofficial/templates/influences-tab-page.hbs",
+					"modules/masks-newgeneration-unofficial/templates/team.hbs",
+					"modules/masks-newgeneration-unofficial/templates/sheets/actor-sheet.hbs",
+					"modules/masks-newgeneration-unofficial/templates/parts/actor-header.hbs",
+					"modules/masks-newgeneration-unofficial/templates/parts/actor-attributes.hbs",
+					"modules/masks-newgeneration-unofficial/templates/parts/actor-movelist.hbs",
+					"modules/masks-newgeneration-unofficial/templates/parts/actor-social.hbs",
+					"modules/masks-newgeneration-unofficial/templates/parts/actor-advancement.hbs",
+					"modules/masks-newgeneration-unofficial/templates/influences-tab-page.hbs",
+				]);
 };
